15db69477d26737624994350968489106466dbef
Siwanont Sittinam <siwanont1@gmail.com>
2017-10-28 23:00:10 +0700

    azure mssql is default db

--- kkhospital/kkhospital/settings.py
+++ kkhospital/kkhospital/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+try:
+   from config import *
+except ImportError:
+   pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -26,7 +30,6 @@ SECRET_KEY = '(dmcaek2381tll^k%p*l1u7r95fha&h0dk1t2=8o0ia!g%uj7l'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,9 +77,16 @@ WSGI_APPLICATION = 'kkhospital.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': SQL_DATABASE,
+        'ENGINE': 'sql_server.pyodbc',
+        'HOST': SQL_HOST,
+        'USER': SQL_USERNAME,
+        'PASSWORD': SQL_PASSWORD,
     }
 }
 
