c43fe417810cc74f684f24628e52097df81dec25
Siwanont Sittinam <siwanont1@gmail.com>
2017-10-29 10:25:26 +0700

    remove sqlite3

--- kkhospital/kkhospital/settings.py
+++ kkhospital/kkhospital/settings.py
@@ -80,10 +80,6 @@ WSGI_APPLICATION = 'kkhospital.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
         'NAME': MSSQL_DATABASE,
         'ENGINE': 'sql_server.pyodbc',
