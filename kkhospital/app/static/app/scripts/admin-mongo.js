90f1163279865644108dcda69142d4b3575cedeb
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2017-11-06 21:12:30 +0700

    control checkbox and delete button on admin mongo page

--- kkhospital/app/static/app/scripts/admin-mongo.js
+++ kkhospital/app/static/app/scripts/admin-mongo.js
@@ -1,13 +1,36 @@
-$('input[type="checkbox"]:first').click(function() {
+// ===============================================================================
+// Control checkbox and delete button
+
+function handleDeleteButton() {
+    var $toolbar = $('.toolbar');
+    if($('td input[type="checkbox"]:checked').length > 0) {
+        $toolbar.find('.delete').removeClass('hide');
+    } else {
+        $toolbar.find('.delete').addClass('hide');
+    }
+}
+
+$('th input[type="checkbox"]').click(function() {
     if($(this).prop('checked')) {
         $('input[type="checkbox"]').prop('checked', true);
     } else {
         $('input[type="checkbox"]').prop('checked', false);
     }
-})
+    handleDeleteButton();
+});
+
+$('td input[type="checkbox"]').click(function() {
+    var $checkedInput = $('td input[type="checkbox"]:checked');
+    if($checkedInput.length === $('td input[type="checkbox"]').length) {
+        $('th input[type="checkbox"]').prop('checked', true);
+    } else {
+        $('th input[type="checkbox"]').prop('checked', false);
+    }
+    handleDeleteButton();
+});
 
 $('input[type="checkbox"]').click(function() {
-    if($('input[type="checkbox"]').length > 0) {
-        // $('button')
-    }
-})
+    handleDeleteButton();
+});
+
+// ===============================================================================
