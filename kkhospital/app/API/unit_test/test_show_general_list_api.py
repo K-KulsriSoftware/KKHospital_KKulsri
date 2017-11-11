2d4063881208c6841237d595238466ed98fa4c79
Watcharachat PLANGSRI <watcharachat.p@live.ku.th>
2017-11-11 15:50:20 +0700

    test_showgenerallist 1st case added

--- kkhospital/app/API/unit_test/test_show_general_list_api.py
+++ kkhospital/app/API/unit_test/test_show_general_list_api.py
@@ -7,7 +7,6 @@ from API import API
 from pprint import pprint
 from datetime import datetime
 
-
 api = API()
 
 list_result = ['package_id',
@@ -22,10 +21,16 @@ class TestUM(unittest.TestCase) :
 
     def test_show_general_list_exist(self) :
         status, result = api.show_general_list()
-        list_key = []
-        for k in result : list_key.append(k)
+        list_key0 = []
+        list_key1 = []
+        list_key2 = []
+        for k in result[0] : list_key0.append(k)
+        for k in result[1] : list_key1.append(k)
+        for k in result[2] : list_key2.append(k)
         self.assertEqual( status ,True )
-        self.assertEqual( sorted(list_key) , sorted(list_result))
+        self.assertEqual( sorted(list_key0) , sorted(list_result))
+        self.assertEqual( sorted(list_key1) , sorted(list_result))
+        self.assertEqual( sorted(list_key2) , sorted(list_result))
         print ('test_show_general_list_exist_use : success')
 
 if __name__ == '__main__' :
